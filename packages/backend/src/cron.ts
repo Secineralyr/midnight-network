cd6ea421d304793a238cb74fbf0e88216096f027
Komasan5364 <61271838+Komasan5364@users.noreply.github.com>
2025-12-29 18:59:59 +0900

    modify: fix some codes

--- packages/backend/src/cron.ts
+++ packages/backend/src/cron.ts
@@ -21,35 +21,46 @@ export async function processCronMain() {
 	const mkApi = createRetryMisskeyApiClientFetcher();
 
 	const targetTimeDate = new Date();
-	targetTimeDate.setUTCHours(15);
-	targetTimeDate.setUTCMinutes(0);
+	targetTimeDate.setUTCHours(env.TARGET_MATCH_HOUR);
+	targetTimeDate.setUTCMinutes(env.TARGET_MATCH_MINUTES);
 	targetTimeDate.setUTCSeconds(0);
+	if (targetTimeDate > new Date()) {
+		targetTimeDate.setDate(targetTimeDate.getDate() - 1);
+	}
 	const targetTime = targetTimeDate.getTime();
 
-	let untilTime = targetTime + 60 * 1000;
+	const untilTime = targetTime + 60 * 1000;
 	const sinceTime = targetTime - 60 * 1000;
 
 	const timeJst = new Date(untilTime + 9 * 3600 * 1000);
-	const monthJst = timeJst.getMonth();
+	const monthJst = timeJst.getUTCMonth();
 	const dayJst = timeJst.getDay();
 
 	let notes: Note[] = [];
 	const targetPattern = new RegExp(`${monthJst}/${dayJst}|${monthJst}月${dayJst}日|${env.GAME_JOIN_POST_TEXT_REGEX}`);
 
+	let untilId: string | null = null;
 	while (true) {
-		const res = await mkApi('notes/hybrid-timeline', {
-			untilDate: untilTime,
+		let paramUntil: Record<string, string | number>;
+		if (untilId === null) {
+			paramUntil = { untilDate: untilTime };
+		} else {
+			paramUntil = { untilId: untilId };
+		}
+		const params = {
 			sinceDate: sinceTime,
 			includeLocalRenotes: false,
 			includeMyRenotes: false,
 			includeRenotedMyNotes: false,
 			limit: 100,
 			withRenotes: false,
-		});
+			...paramUntil,
+		};
+		const res = await mkApi('notes/hybrid-timeline', params);
 		if (res.length === 0) {
 			break;
 		}
-		untilTime = res.map((n) => Date.parse(n.createdAt)).reduce((t, next) => (t < next ? t : next), untilTime);
+		untilId = res.at(-1)?.id ?? null;
 		const addend = res.filter((note) => note.text && targetPattern.test(note.text) && !(note.user.isBot ?? false));
 		notes = notes.concat(addend);
 	}
@@ -57,7 +68,7 @@ export async function processCronMain() {
 	// データ整理
 	const users: Record<string, string> = {};
 	let records: Record<string, { uid: string; nid: string; postedAt: Date; dt: number; place: number }> = {};
-	notes.forEach((n) => {
+	for (const n of notes) {
 		const uid = n.userId;
 		const dt = Date.parse(n.createdAt) - targetTime;
 		if (!records[uid] || records[uid].dt > dt) {
@@ -70,7 +81,7 @@ export async function processCronMain() {
 			};
 		}
 		users[uid] = n.user.username;
-	});
+	}
 
 	const validRecords = [...Object.values(records)].filter((n) => n.dt >= 0);
 	const flyingRecords = [...Object.values(records)].filter((n) => n.dt < 0);
@@ -79,39 +90,70 @@ export async function processCronMain() {
 	const flyingCount = [...Object.values(records)].length - validCount;
 
 	// 結果をノートする
-	const host = env.WEB_HOST;
-	const noteTitle = env.POST_MATCH_RESULT_TITLE;
-	const noteUrl = env.POST_MATCH_RESULT_URL.replace('{host}', host);
+	if (!env.DISABLE_POST_MATCH_RESULT) {
+		const host = env.WEB_HOST;
+		const noteTitle = env.POST_MATCH_RESULT_TITLE;
+		const noteUrl = env.POST_MATCH_RESULT_URL.replace('{host}', host);
 
-	const ranking: string[] = [];
-	const dts = [...new Set<number>(validRecords.map((r) => r.dt))];
-	dts.sort((a, b) => a - b);
-	for (let i = 0; i < dts.length; i++) {
-		validRecords
-			.filter((r) => r.dt === dts[i])
-			.forEach((r) => {
-				r.place = i + 1;
-				if (i < 10) {
-					let mention = '';
-					if (users[r.uid]) {
-						mention = `@${users[r.uid]}`;
+		const ranking: string[] = [];
+		const dts = [...new Set<number>(validRecords.map((r) => r.dt))];
+		dts.sort((a, b) => a - b);
+		for (let i = 0; i < dts.length; i++) {
+			validRecords
+				.filter((r) => r.dt === dts[i])
+				.forEach((r) => {
+					r.place = i + 1;
+					if (i < 10) {
+						let mention = '';
+						if (users[r.uid]) {
+							mention = `@${users[r.uid]}`;
+						}
+						ranking.push(`${placeEmojis[i + 1]} ${mention} +${(r.dt / 1000).toFixed(3)}s`);
 					}
-					ranking.push(`${placeEmojis[i + 1]} ${mention} +${(r.dt / 1000).toFixed(3)}s`);
-				}
-			});
+				});
+		}
+		const rankText = ranking.join('\n');
+
+		const resultText = env.POST_MATCH_RESULT_TEMPLATE.replace('{title}', noteTitle)
+			.replace('{ranks}', rankText)
+			.replace('{valid}', validCount.toFixed(0))
+			.replace('{flying}', flyingCount.toFixed(0))
+			.replace('{url}', noteUrl);
+
+		await mkApi('notes/create', { text: resultText });
 	}
-	const rankText = ranking.join('\n');
-
-	const resultText = env.POST_MATCH_RESULT_TEMPLATE.replace('{title}', noteTitle)
-		.replace('{ranks}', rankText)
-		.replace('{valid}', validCount.toFixed(0))
-		.replace('{flying}', flyingCount.toFixed(0))
-		.replace('{url}', noteUrl);
-
-	await mkApi('notes/create', { text: resultText });
 
 	// DBデータ全般更新
-	const matchDate = await prisma.matchDate.create({ data: { date: targetTimeDate } });
+	const eventMatch = await prisma.eventMatch.findFirst({
+		where: {
+			AND: {
+				startDate: {
+					lte: targetTimeDate,
+				},
+				endDate: {
+					gte: targetTimeDate,
+				},
+			},
+		},
+	});
+	let eventData: Record<string, number> = {};
+	if (eventMatch !== null) {
+		eventData = { eventId: eventMatch.id };
+	}
+
+	const matchDate = await prisma.matchDate.upsert({
+		where: {
+			date: targetTimeDate,
+		},
+		update: {
+			date: targetTimeDate,
+			...eventData,
+		},
+		create: {
+			date: targetTimeDate,
+			...eventData,
+		},
+	});
 
 	for (const uid in users) {
 		const username = users[uid];
@@ -131,8 +173,17 @@ export async function processCronMain() {
 	}
 
 	for (const rec of [...validRecords, ...flyingRecords]) {
-		await prisma.record.create({
-			data: {
+		await prisma.record.upsert({
+			where: {
+				noteId: rec.nid,
+			},
+			update: {
+				postedAt: rec.postedAt,
+				userId: rec.uid,
+				place: rec.place,
+				matchDateId: matchDate.id,
+			},
+			create: {
 				noteId: rec.nid,
 				postedAt: rec.postedAt,
 				userId: rec.uid,
@@ -199,6 +250,11 @@ export async function processCronMain() {
 		};
 
 		let event: RankCalculationEvent;
+		let eventMultiplier = 1.0;
+		if (eventMatch !== null) {
+			eventMultiplier = eventMatch.multiplePt;
+		}
+
 		if (records[uid]) {
 			if (records[uid].dt >= 0) {
 				event = {
@@ -207,6 +263,7 @@ export async function processCronMain() {
 					placement: records[uid].place,
 					timeSeconds: records[uid].dt,
 					withinZoneParticipants: withinZoneParticipants,
+					eventMultiplier: eventMultiplier,
 				} satisfies RankCalculationParticipatedEvent;
 			} else {
 				event = {
@@ -246,8 +303,29 @@ export async function processCronMain() {
 }
 
 export async function processCronRemind() {
+	const targetTimeDate = new Date();
+	targetTimeDate.setUTCHours(env.TARGET_MATCH_HOUR);
+	targetTimeDate.setUTCMinutes(env.TARGET_MATCH_MINUTES);
+	targetTimeDate.setUTCSeconds(0);
+	if (targetTimeDate < new Date()) {
+		targetTimeDate.setDate(targetTimeDate.getDate() + 1);
+	}
+
+	const targetTime = targetTimeDate.getTime();
+
+	const sinceTime = targetTime - 60 * 1000;
+	const untilTime = targetTime + 60 * 1000;
+
+	const sinceDate = new Date(sinceTime).toLocaleDateString('ja-JP');
+	const untilDate = new Date(untilTime).toLocaleDateString('ja-JP');
+
+	let crossOverDaysText = '';
+	if (sinceDate != untilDate) {
+		crossOverDaysText = `(${sinceDate} -> ${untilDate})`;
+	}
+
 	const mkApi = createRetryMisskeyApiClientFetcher();
-	await mkApi('notes/create', { text: env.POST_MATCH_REMIND_TEXT });
+	await mkApi('notes/create', { text: `${env.POST_MATCH_REMIND_TEXT}${crossOverDaysText}` });
 	return;
 	// TODO: リマインド用ノート投稿タスク
 }
