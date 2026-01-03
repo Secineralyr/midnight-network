ddf6e98487b5220d22d4ecafa05335c86c6fede6
slofp <phy.public@gmail.com>
2026-01-04 04:34:24 +0900

    fix: user add middleware

--- packages/backend/src/rpc/helpers/cache.ts
+++ packages/backend/src/rpc/helpers/cache.ts
@@ -93,7 +93,7 @@ export async function setToCache<T>(key: string, value: T, ttlSeconds: number): 
 	if (serialized === undefined) {
 		return;
 	}
-	await env.CACHE.put(key, serialized, { expirationTtl: ttlSeconds });
+	await withTimeout(env.CACHE.put(key, serialized, { expirationTtl: ttlSeconds }), undefined);
 }
 
 /**
