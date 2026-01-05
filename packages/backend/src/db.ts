808b08d6b63629344020f9bd59b9ae412a4a987c
slofp <phy.public@gmail.com>
2026-01-06 04:35:54 +0900

    feat: debug test query logging

--- packages/backend/src/db.ts
+++ packages/backend/src/db.ts
@@ -3,4 +3,30 @@ import { PrismaD1 } from '@prisma/adapter-d1';
 import { PrismaClient } from './generated/prisma/client';
 
 const adapter = new PrismaD1(env.DB);
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient({
+	adapter,
+	log: [
+		{
+			emit: 'event',
+			level: 'query',
+		},
+		{
+			emit: 'stdout',
+			level: 'error',
+		},
+		{
+			emit: 'stdout',
+			level: 'info',
+		},
+		{
+			emit: 'stdout',
+			level: 'warn',
+		},
+	],
+});
+
+prisma.$on('query', (e) => {
+	console.info(`Query: ${e.query}`);
+	console.info(`Params: ${e.params}`);
+	console.info(`Duration: ${e.duration}ms`);
+});
