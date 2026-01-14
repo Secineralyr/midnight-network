1d67c48b37ec33375f57146378fa5736b23ff4f5
slofp <phy.public@gmail.com>
2026-01-14 10:06:19 +0900

    fix: fetch credentials include

--- packages/frontend/src/lib/orpc.ts
+++ packages/frontend/src/lib/orpc.ts
@@ -4,6 +4,11 @@ import { RPCLink } from '@orpc/client/fetch';
 
 const link = new RPCLink({
 	url: () => `${import.meta.env.VITE_API_ROOT}/api`,
+	fetch: (request, init) =>
+		fetch(request, {
+			...init,
+			credentials: 'include',
+		}),
 });
 
 export const orpc: AppClient = createORPCClient(link);
