d952b527d855c4498dd8509185ea02c9b802665b
slofp <phy.public@gmail.com>
2026-01-20 03:20:41 +0900

    fix: transaction

--- packages/backend/src/cron.ts
+++ packages/backend/src/cron.ts
@@ -24,6 +24,7 @@ import type {
 	UserRankStatusUpdateArgs,
 	UserSettingsCreateManyInput,
 } from './generated/prisma/models';
+import type { PrismaPromise } from './generated/prisma/internal/prismaNamespace';
 
 function getTargetTimeRange(): [number, number] {
 	const targetTime = getTargetTime();
@@ -190,45 +191,51 @@ async function upsertMatchResultData(
 		}
 	}
 
+	const queryTasks: PrismaPromise<unknown>[] = [];
 	console.info('cron.mainProcess: insert new user');
 	if (mustCreateUsers.length > 0) {
 		console.info(`cron.mainProcess: new user length ${mustCreateUsers.length}`);
-		await prisma.user.createMany({ data: mustCreateUsers });
+		queryTasks.push(prisma.user.createMany({ data: mustCreateUsers }));
 	}
 
 	console.info('cron.mainProcess: insert new user status');
 	if (mustCreateStatus.length > 0) {
 		console.info(`cron.mainProcess: new user status length ${mustCreateUsers.length}`);
-		await prisma.userRankStatus.createMany({ data: mustCreateStatus });
+		queryTasks.push(prisma.userRankStatus.createMany({ data: mustCreateStatus }));
 	}
 
 	console.info('cron.mainProcess: insert new user settings');
 	if (mustCreateSettings.length > 0) {
 		console.info(`cron.mainProcess: new user settings length ${mustCreateUsers.length}`);
-		await prisma.userSettings.createMany({ data: mustCreateSettings });
+		queryTasks.push(prisma.userSettings.createMany({ data: mustCreateSettings }));
 	}
 
 	console.info('cron.mainProcess: insert record');
 	for (const rec of [...validRecords, ...flyingRecords]) {
-		await prisma.record.upsert({
-			where: {
-				noteId: rec.nid,
-			},
-			update: {
-				postedAt: rec.postedAt,
-				userId: rec.uid,
-				place: rec.place,
-				matchDateId: matchDate.id,
-			},
-			create: {
-				noteId: rec.nid,
-				postedAt: rec.postedAt,
-				userId: rec.uid,
-				place: rec.place,
-				matchDateId: matchDate.id,
-			},
-		});
+		queryTasks.push(
+			prisma.record.upsert({
+				where: {
+					noteId: rec.nid,
+				},
+				update: {
+					postedAt: rec.postedAt,
+					userId: rec.uid,
+					place: rec.place,
+					matchDateId: matchDate.id,
+				},
+				create: {
+					noteId: rec.nid,
+					postedAt: rec.postedAt,
+					userId: rec.uid,
+					place: rec.place,
+					matchDateId: matchDate.id,
+				},
+			}),
+		);
 	}
+
+	console.info('cron.mainProcess: record transaction execute');
+	await prisma.$transaction(queryTasks);
 
 	console.info('cron.mainProcess: record process ok');
 	return {
@@ -269,12 +276,13 @@ async function upsertRankResultData(
 			).rankNumber,
 		}));
 
+	const userRankStatuses = await prisma.userRankStatus.findMany({ where: { id: { in: allUsers.map((v) => v.id) } } });
 	const createRankHistories: UserRankHistoryCreateManyInput[] = [];
 	const updateRankStatusData: UserRankStatusUpdateArgs[] = [];
 	for (const user of allUsers) {
 		const uid = user.id;
-		let userRankStatus = await prisma.userRankStatus.findUnique({ where: { id: uid } });
-		if (userRankStatus === null) {
+		let userRankStatus = userRankStatuses.find((v) => v.id === uid);
+		if (userRankStatus === undefined) {
 			// 基本的に発生し得ないが、予防処理
 			userRankStatus = await prisma.userRankStatus.create({ data: { id: uid } });
 		}
@@ -346,12 +354,16 @@ async function upsertRankResultData(
 		});
 	}
 
+	const queryTasks: PrismaPromise<unknown>[] = [];
 	console.info('cron.mainProcess: create histories');
 	if (createRankHistories.length > 0) {
-		await prisma.userRankHistory.createMany({ data: createRankHistories });
+		queryTasks.push(prisma.userRankHistory.createMany({ data: createRankHistories }));
 	}
 	console.info('cron.mainProcess: update rank status');
-	await Promise.all(updateRankStatusData.map((v) => prisma.userRankStatus.update(v)));
+	queryTasks.push(...updateRankStatusData.map((v) => prisma.userRankStatus.update(v)));
+
+	console.info('cron.mainProcess: rank transaction execute');
+	await prisma.$transaction(queryTasks);
 }
 
 export async function processCronMain() {
