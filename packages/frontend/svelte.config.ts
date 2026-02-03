beba140e260daa4c21122786ab191cdf615075be
slofp <phy.public@gmail.com>
2026-02-03 20:54:44 +0900

    feat: add mdsvex

--- packages/frontend/svelte.config.ts
+++ packages/frontend/svelte.config.ts
@@ -1,9 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
 import type { Config } from '@sveltejs/kit';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
+
+const mdsvexPreprocessor = mdsvex({ extensions: ['.md'] });
 
 const config: Config = {
-	preprocess: vitePreprocess(),
+	extensions: ['.svelte', '.md'],
+	preprocess: [
+		vitePreprocess(),
+		{
+			...mdsvexPreprocessor,
+			markup: ({ content, filename }) => mdsvexPreprocessor.markup({ content, filename: filename ?? '' }),
+		},
+	],
 	kit: {
 		adapter: adapter({
 			pages: 'dist',
